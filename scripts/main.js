7ee2e7e02fa2b821731b29bd5a268160876dbb61
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2024-07-23 12:50:06 +0200

    Add `Personal Info` and `Contact Info`
    Signed-off-by: FedericoBruzzone <federico.bruzzone.i@gmail.com>

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,3 @@
 // <script type="module" src="/scripts/main.js" defer></script>
 import '/scripts/header-loader.js';
 import '/scripts/footer-loader.js';
-import '/scripts/navbar.js';
--- scripts/navbar.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    fetch('/includes/navbar.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('navbar').innerHTML = data;
-        })
-        .catch(error => console.error('Error loading navbar:', error));
-});
-
